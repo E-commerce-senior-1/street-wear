8095d2d42b6845f57b281c431385b7b364c53f64
khadhKadh <azizkh103@gmail.com>
2023-12-13 15:25:31 +0100

    Navbar and footer done missing the cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from './Components/NavBar/NavBar';
+import Artist from './Components/artist/Artist';
+import Footer from './Components/NavBar/Footer';
 const App = () => {
   return (
-    <div>
-      
-    </div>
+
+    <Router>
+        <NavBar/>   
+      <Routes>
+        <Route path='/'/>
+        <Route path='/home'/>
+        <Route path='/PeronalCollection'/>
+        <Route path='/Drops'/>
+        <Route path='Profile'/>
+        <Route path='SignIn'/>
+        <Route path='SignUp'/>
+        <Route path='Aboutus'/>
+        <Route path='/stats'/>
+      </Routes>
+      <Footer/>
+    </Router>
+
   )
 }
 
