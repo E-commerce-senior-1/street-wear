bf46a8c5fc7c6faf4c9c10caf4abc9a62d2bd285
Mohamed <hasanbouhlel@gmail.com>
2023-12-15 16:16:21 +0100

    switched back to my branch smoothly

--- server/route/artist.js
+++ server/route/artist.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const {getUserData , addPost , updateName , updateEmail , updateBio , updatePfp , updateCoverPic} = require("../Controller/user/artist")
 
-
-
+router.get("/Profile/:email" , getUserData )
+router.post("/Profile/Post/:idartist" , addPost )
+router.put("/Profile/Name/:id" , updateName)
+router.put("/Profile/Email/:id" , updateEmail)
+router.put("/Profile/Bio/:id" , updateBio)
+router.put("/Profile/ProfilePic/:id" , updatePfp)
+router.put("/Profile/CoverPic/:id" , updateCoverPic)
 
 module.exports = router
